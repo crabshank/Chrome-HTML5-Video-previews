31e5ee13a92dadcb50d51c5b65b3b0a502eba223
crabshank <44329154+crabshank@users.noreply.github.com>
2021-03-07 23:38:27 +0000

    Add files via upload

--- background.js
+++ background.js
@@ -1,36 +1,43 @@
-var sc=0;
-var sync=[];
-var dly=0;
-var msg=0;
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log(request);
-	switch (request.message){
-		case "Flush!":
-		sc=0;
-			msg={message:"Flush!"};
-		 for (var i = 0, len = sync.length; i < len; i++){
-	chrome.tabs.sendMessage(sync[i].sender.tab.id,msg);
-		 }
-		sync=[];
-
-		break;
+try {
+function getUrl(tab){
+	if(tab.pendingUrl!=null){
+	return (tab.url=="")?tab.pendingUrl:tab.url;
+	}else{
 		
-	case "Buttons created!":
-		msg={message:"Buttons creation noted!"};
-	chrome.tabs.sendMessage(sender.tab.id,msg);
-	break;
-/*
-	case "sEvt":
-		msg=request;
-		msg.dly=dly;
-		 for (var i = 0, len = sync.length; i < len; i++){
-	chrome.tabs.sendMessage(sync[i].sender.tab.id,msg);
-		 }
-	break;
-*/
+		if(tab.url!=null){
+			return tab.url;
+		}else{
+			return tab; //if tab.url not there
+		}
+	
+	}
+}
+function send(message) {
 
-	default:
-	console.log(request);
-	break;
-	 }
-	});
+    let params = {
+      active: true,
+      currentWindow: true
+    }
+    chrome.tabs.query(params, gotTabs);
+
+    function gotTabs(tabs) {
+      console.log("got tabs");
+      console.log(tabs);
+      // send a message to the content script
+     // let message = userinput.value();
+      let msg = {
+        message: message
+      };
+      chrome.tabs.sendMessage(tabs[0].id, msg);
+    }
+
+  }
+
+chrome.action.onClicked.addListener((tab) => {
+  send(getUrl(tab));
+});
+
+
+} catch (e) {	
+  console.error(e);
+}
