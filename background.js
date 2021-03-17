efa72f7225b46a0a27336136c60d2275333bad2b
crabshank <44329154+crabshank@users.noreply.github.com>
2021-03-17 16:26:04 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 try {
 function getUrl(tab){
-	return (tab.url=="" && typeof tab.pendingUrl!=='undefined' && tab.pendingUrl!='')?tab.pendingUrl:tab.url;
+	return (tab.url=="" && !!tab.pendingUrl && typeof tab.pendingUrl!=='undefined' && tab.pendingUrl!='')?tab.pendingUrl:tab.url;
 }
 
 function send(message,dims,tabId) {
