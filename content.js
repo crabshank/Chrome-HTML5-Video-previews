8268a2d108278a4e5e3ef50e8c2622c6a8d013ca
crabshank <44329154+crabshank@users.noreply.github.com>
2023-01-05 03:44:44 +0000

    Add files via upload

--- content.js
+++ content.js
@@ -48,7 +48,9 @@ while(srCnt<shrc_l){
 		out.push(...nk);
 	}
 	
-	shrc.push(...curr.childNodes);
+	for(let i=0, len=curr.childNodes.length; i<len; i++){
+		shrc.push(curr.childNodes[i]);
+	}
 	
 	if(sh){
 		   let cs=curr.shadowRoot;
